3ec1d785f7d23549e7432b85da18ffd38dc76ece
McLovinCSUN <60550200+McLovinCSUN@users.noreply.github.com>
2020-08-08 22:47:43 -0700

    update
    added weather api and geolocation api

--- wireframe_to_HTML-CSS/index.js
+++ wireframe_to_HTML-CSS/index.js
@@ -38,11 +38,7 @@ function search(event) {
   } else {
     updateCity.innerHTML = `Please type a city...`;
   }
-}
-
-function changeTemp(event) {
-  event.preventDefault();
-  tempC.innerHTML = "28.3 ℃";
+  searchWeather(searchInputToLower);
 }
 
 let updateCity = document.querySelector("#currentCity");
@@ -51,3 +47,48 @@ form.addEventListener("submit", search);
 
 let tempC = document.querySelector("#temp");
 tempC.addEventListener("click", changeTemp);
+let latitude;
+let longitude;
+let apiKey = "4c362e7f4f84ef4ab0ee164594102485";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+
+function handlePosition(response) {
+  latitude = response.coords.latitude;
+  longitude = response.coords.longitude;
+  axios
+    .get(
+      `${apiUrl}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`
+    )
+    .then(getWeather);
+}
+
+function getWeather(response) {
+  let tempF = document.querySelector("#temp");
+  let currentCity = document.querySelector("#currentCity");
+  let city = response.data.name;
+  let temp = response.data.main.temp;
+  currentCity.innerHTML = city;
+  tempF.innerHTML = `${Math.round(temp)}℉`;
+}
+
+function searchWeather(response) {
+  axios
+    .get(`${apiUrl}q=${response}&appid=${apiKey}&units=imperial`)
+    .then(getSearchWeather);
+}
+
+function getSearchWeather(response) {
+  let tempF = document.querySelector("#temp");
+  let temp = response.data.main.temp;
+  tempF.innerHTML = `${Math.round(temp)}℉`;
+}
+
+function changeTemp(event) {
+  event.preventDefault();
+  tempC.innerHTML = "28.3 ℃";
+}
+function click() {
+  navigator.geolocation.getCurrentPosition(handlePosition);
+}
+let currentLocation = document.querySelector("#currentLocation");
+currentLocation.addEventListener("click", click);
